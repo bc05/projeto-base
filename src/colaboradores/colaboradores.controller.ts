44101cd51c649f01fb9ca7189c9108ad01e09e9f
Bc05 <odilon.garcez.moraes@gmail.com>
2022-02-22 09:51:23 -0300

    refactor: definindo nome da rota pelo nome da coleção
    Co-authored-by: BrunoSantosSousa <BrunoSantosSousa@users.noreply.github.com>
    Co-authored-by: Matheus Brito <FelippeBritto@users.noreply.github.com>

--- src/colaboradores/colaboradores.controller.ts
+++ src/colaboradores/colaboradores.controller.ts
@@ -1,12 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+
 import { RespostaSimplesDto } from '../comum/dtos/resposta.dto';
 import { mensagemResposta, TipoOperacaoCRUD } from '../comum/mensagens';
+import { nomeColaboradoresSchema } from './colaborador.schema';
 import { ColaboradoresService } from './colaboradores.service';
-
 import { CriarColaboradorDto } from './criar-colaborador.dto';
 
-@Controller('colaboradores') // pegar do schema?
+@Controller(nomeColaboradoresSchema)
 export class ColaboradoresController {
   constructor(private service: ColaboradoresService) {}
 
