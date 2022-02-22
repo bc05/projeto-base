8b0fefd19153cdb97600b84768e17a161412a188
Bc05 <odilon.garcez.moraes@gmail.com>
2022-02-22 09:52:16 -0300

    refactor: alterado nome do type do Schema e Document

--- src/colaboradores/colaboradores.repository.ts
+++ src/colaboradores/colaboradores.repository.ts
@@ -7,26 +7,26 @@ import {
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
-import { Colaborador, ColaboradorDocumento } from './colaborador.schema';
+import { Colaborador, ColaboradorDocument } from './colaborador.schema';
 import { IContrarPorFiltros } from './contar-filtros.interface';
 import { CriarColaboradorDto } from './criar-colaborador.dto';
 
 interface IColaboradoresRepository
-  extends IRepositoryCriar<CriarColaboradorDto, ColaboradorDocumento>,
+  extends IRepositoryCriar<CriarColaboradorDto, ColaboradorDocument>,
     IRepositoryContar<IContrarPorFiltros> {}
 
 @Injectable()
 export class ColaboradoresRepository implements IColaboradoresRepository {
   constructor(
     @InjectModel(Colaborador.name)
-    private colaboradorModel: Model<ColaboradorDocumento>,
+    private colaboradorModel: Model<ColaboradorDocument>,
   ) {}
 
   async contarPorFiltros(filtros: IContrarPorFiltros): Promise<number> {
     return this.colaboradorModel.countDocuments(filtros).exec();
   }
 
-  async criar(dados: CriarColaboradorDto): Promise<ColaboradorDocumento> {
+  async criar(dados: CriarColaboradorDto): Promise<ColaboradorDocument> {
     return this.colaboradorModel.create(dados);
   }
 }
