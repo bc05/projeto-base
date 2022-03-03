882d2e10be5c4c832739c61a8185598ff839de0d
Bc05 <odilon.garcez.moraes@gmail.com>
2022-03-03 14:13:54 -0300

    feat: uso do filtro global de validação de request

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -20,6 +20,12 @@ function iniciarSwagger(app: INestApplication): void {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   iniciarSwagger(app);
 
